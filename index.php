08a0f03319499eb6392a6bc9bc3006ddcb3d192c
Andi_biasa_ajah <andinata1991@gmail.com>
2023-05-11 16:56:37 +0700

    menambakan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum voluptatibus voluptates autem inventore ab,
         corrupti reiciendis, cupiditate iure mollitia, laborum ducimus beatae nemo dolores. Itaque laboriosam distinctio
         quidem minima nihil.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
